18871f78600463969454ebb78683fa44df1fe946
fadlan <fadlanmulya@gmail.com>
2022-09-11 17:20:25 +0700

    update role

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -111,7 +111,7 @@ class NilaiController extends Controller
             
 
             // $user_id = User::select('id', 'id as user_id');
-            $user_id = Ustad::all()->pluck('id')->where('user_id', 2);
+            $user_id = Ustad::all()->pluck('id')->when('user_id', auth()->user()->id);
             // $validatedData['ustad_id'] = Ustad::where('user_id', $user_id)->pluck('id');
             dd($user_id);
         }
