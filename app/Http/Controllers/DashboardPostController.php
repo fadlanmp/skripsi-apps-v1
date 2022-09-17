ee797ca5498f0cb29605661fab1e29739369ff5d
fadlanmp <fadlanmulya@gmail.com>
2022-09-17 08:51:38 +0700

    upload image blog

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -131,7 +131,7 @@ class DashboardPostController extends Controller
             if($request->oldImage){
                 Storage::delete($request->oldImage);
             }
-            $validatedData['image'] = Cloudinary::upload($request->file('image')->getRealPath(), 'blog')->getSecurePath();
+            $validatedData['image'] = Cloudinary::upload($request->file('image')->storeOnCloudinary('blog')->getRealPath())->getSecurePath();
             dd($validatedData);
         }
         
