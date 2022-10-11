449a358448518923325b7ffb5eb11b4f3babc300
fadlanmp <fadlanmulya@gmail.com>
2022-10-12 06:06:07 +0700

    perbaiki tampilan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -78,7 +78,7 @@ class DashboardController extends Controller
                 $mapel[] = $k->title;
                 $nilai[] = Nilai::where('kitab_id', $k->id, '&&', 'santri_id',$santri_id);
             }
-            dd($mapel);
+            dd($nilai);
 
             return view('admin.dashboard',[
                 'title' => 'Dashboard',
