704ca259b03226d26943e53cd2148d41d3662d2c
fadlanmp <fadlanmulya@gmail.com>
2022-10-12 04:50:01 +0700

    perbaiki tampilan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -6,9 +6,9 @@
     <h1 class="h2">Welcome back, {{ auth()->user()->name }}</h1>
   </div>
   
-  @canany(['admin', 'ustad'])
+  {{-- @canany(['admin', 'ustad'])
     <div id="civitas"></div>
-  @endcanany
+  @endcanany --}}
 
   {{-- @can('santri')
     <div id="civitasSantri"></div>
