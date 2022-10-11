925c03df40579eaa15eb788beb70e0ab64619c3d
fadlanmp <fadlanmulya@gmail.com>
2022-10-12 04:23:50 +0700

    perbaiki tampilan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -182,8 +182,6 @@
       },
       tooltip: {
           headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-          pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-              '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
           footerFormat: '</table>',
           shared: true,
           useHTML: true
@@ -200,7 +198,7 @@
       },
       {
         name: 'Ustad',
-        data: [3,8]
+        data: [{!! json_encode($ustadlk) !!},{!! json_encode($ustadptr) !!}]
       }]
   });
   </script>
