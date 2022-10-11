d4253ea4cef47d2da1b41ee71e7217bf33a5c77f
fadlanmp <fadlanmulya@gmail.com>
2022-10-12 04:05:26 +0700

    perbaiki tampilan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,8 +17,8 @@ class DashboardController extends Controller
     {
 
         if(Gate::allows('admin')){
-            $santri = Santri::all();
-            $santri->loadCount('jk');
+            $santri = Santri::first();
+            // $santri->loadCount('jk');
             dd($santri);
             
             return view('admin.dashboard',[
