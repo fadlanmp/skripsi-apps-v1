7a6e5b32f0b9d5e3ef3c9f13de15bbf6d508194e
fadlanmp <fadlanmulya@gmail.com>
2022-10-12 03:15:40 +0700

    perbaiki tampilan

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -165,34 +165,19 @@
           type: 'column'
       },
       title: {
-          text: 'Emissions to air in Norway'
-      },
-      subtitle: {
-          text: 'Source: ' +
-              '<a href="https://www.ssb.no/en/statbank/table/08940/" ' +
-              'target="_blank">SSB</a>'
+          text: 'Rekap Santri Berdasarkan Jenis Kelamin'
       },
       xAxis: {
           categories: [
-              '2010',
-              '2011',
-              '2012',
-              '2013',
-              '2014',
-              '2015',
-              '2016',
-              '2017',
-              '2018',
-              '2019',
-              '2010',
-              '2021'
+              'Laki-laki',
+              'Perempuan'
           ],
           crosshair: true
       },
       yAxis: {
           title: {
               useHTML: true,
-              text: 'Million tonnes CO<sub>2</sub>-equivalents'
+              text: 'Jumlah'
           }
       },
       tooltip: {
@@ -210,24 +195,8 @@
           }
       },
       series: [{
-          name: 'Oil and gas extraction',
-          data: [13.93, 13.63, 13.73, 13.67, 14.37, 14.89, 14.56,
-              14.32, 14.13, 13.93, 13.21, 12.16]
-
-      }, {
-          name: 'Manufacturing industries and mining',
-          data: [12.24, 12.24, 11.95, 12.02, 11.65, 11.96, 11.59,
-              11.94, 11.96, 11.59, 11.42, 11.76]
-
-      }, {
-          name: 'Road traffic',
-          data: [10.00, 9.93, 9.97, 10.01, 10.23, 10.26, 10.00,
-              9.12, 9.36, 8.72, 8.38, 8.69]
-
-      }, {
-          name: 'Agriculture',
-          data: [4.35, 4.32, 4.34, 4.39, 4.46, 4.52, 4.58, 4.55,
-              4.53, 4.51, 4.49, 4.57]
+          name: 'Santri',
+          data: [13.93, 13.63]
 
       }]
   });
