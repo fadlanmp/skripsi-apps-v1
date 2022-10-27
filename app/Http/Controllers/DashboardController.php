36f2402a77fa013f4bda912fbc540a94f525311f
fadlanmp <fadlanmulya@gmail.com>
2022-10-28 00:23:02 +0700

    perbaikan dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -44,7 +44,7 @@ class DashboardController extends Controller
             $nilai = [];
             foreach($kitabs as $k){
                 $kitab[] = $k->title;
-                $nilai[] = Nilai::where('kitab_id', $k->id)->avg('nilai')->is_float;
+                $nilai[] = (float) Nilai::where('kitab_id', $k->id)->avg('nilai');
             }
 
             dd($nilai);
